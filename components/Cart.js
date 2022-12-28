e3c90c4afb051792b94ae5ac64648d415c364726
Thet Hmuu <thethmuueinsoe@gmail.com>
2022-12-28 18:56:44 +0630

    Stripe setup and initial integration

--- components/Cart.js
+++ components/Cart.js
@@ -5,10 +5,24 @@ import { FaShoppingCart } from 'react-icons/fa';
 import { AiFillPlusCircle, AiFillMinusCircle } from 'react-icons/ai';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
+import connectStripe from 'lib/connectStripe';
 
 const Cart = () => {
   const { setShowCart, cartItems, handleOnAdd, handleOnRemove, totalPrice } =
     useStoreContext();
+
+  const handleCheckout = async () => {
+    const stripe = await connectStripe();
+    const res = await fetch('/api/stripe', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(cartItems),
+    });
+    const data = await res.json();
+    console.log(data);
+    await stripe.redirectToCheckout({ sessionId: data.id });
+  };
+
   return (
     <CartWrapper
       as={motion.div}
@@ -72,7 +86,7 @@ const Cart = () => {
           {cartItems.length > 0 ? (
             <div>
               <h3>Subtotal ${totalPrice}</h3>
-              <button>Purchase</button>
+              <button onClick={handleCheckout}>Purchase</button>
             </div>
           ) : null}
         </Checkout>
---
+++ pages/api/stripe.js
@@ -0,0 +1,35 @@
+import { isTypeNode } from 'graphql';
+
+const stripe = require('stripe')(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+
+export default async function stripeHandler(req, res) {
+  if (req.method === 'POST') {
+    try {
+      const session = await stripe.checkout.sessions.create({
+        mode: 'payment',
+        payment_method_types: ['card'],
+        line_items: req.body.map((item) => {
+          return {
+            price_data: {
+              currency: 'usd',
+              product_data: {
+                name: item.title,
+                images: [item.image.data.attributes.formats.thumbnail.url],
+              },
+              unit_amount: item.price * 100,
+            },
+            quantity: item.quantity,
+          };
+        }),
+        success_url: `${req.headers.origin}/success`,
+        cancel_url: `${req.headers.origin}/canceled`,
+      });
+      res.status(200).json(session);
+    } catch (error) {
+      res.status(error.statusCode || 500).json(error.message);
+    }
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method not allowed');
+  }
+}
