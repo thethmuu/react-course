32e33d6289d7afd57b839587ab8d568b769030ab
Thet Hmuu <thethmuueinsoe@gmail.com>
2023-01-02 20:01:53 +0630

    auth0 initial setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import { createClient, Provider } from 'urql';
-import Head from 'next/head';
+import { UserProvider } from '@auth0/nextjs-auth0/client';
 import 'styles/globals.css';
 import { StateContextProvider } from 'lib/context';
 import Navbar from 'components/Navbar';
@@ -10,12 +10,14 @@ const client = createClient({ url: process.env.NEXT_PUBLIC_GRAPHQL_URL });
 
 export default function App({ Component, pageProps }) {
   return (
-    <StateContextProvider>
-      <Provider value={client}>
-        <UserLayout>
-          <Component {...pageProps} />
-        </UserLayout>
-      </Provider>
-    </StateContextProvider>
+    <UserProvider>
+      <StateContextProvider>
+        <Provider value={client}>
+          <UserLayout>
+            <Component {...pageProps} />
+          </UserLayout>
+        </Provider>
+      </StateContextProvider>
+    </UserProvider>
   );
 }
