357c816b99718192a6ae649fd54bded64a19486e
Thet Hmuu <thethmuueinsoe@gmail.com>
2023-04-27 11:07:50 +0630

    removed uncessary page

--- pages/jobs/index.jsx
+++
@@ -1,9 +0,0 @@
-import { useRouter } from 'next/router';
-import React from 'react';
-
-export default function Index() {
-  const router = useRouter();
-
-  console.log(router.query.categories);
-  return <div>Jobs by category</div>;
-}
